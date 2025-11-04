18bf3c00828d28205281bbea14e676c76ee84c4f
Ratnesh Rastogi <ratneshrastogimarch@gmail.com>
2025-11-04 10:27:46 +0000

    Fix data seeding script

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -20,19 +20,20 @@ async function seedSchools() {
         INSERT INTO schools
             (id, name, address, city, state, contact, image_url, email_id)
         VALUES
-        (21,'est','1936 O\'Reilly Estates Suite 665\nEast Leonelbury, DC 90308','Port Ileneberg','NewHampshire',0,'http://hudson.info/','weichmann@hotmail.com'),
-        (22,'hic','01200 Lebsack Lakes\nEast Jake, ND 98136','Zemlakchester','California',77,'http://www.bauch.com/','allene29@hotmail.com'),
-        (24,'nihil','14576 Lelah Drive Suite 966\nNew Chanelhaven, KS 94846','East Jovanyberg','Utah',1179086025,'http://www.kuphal.com/','abraham31@gmail.com'),
-        (25,'at','0975 Jaylan Manor Suite 103\nPort Trent, UT 68256-3799','Port Kelliside','NorthCarolina',1,'http://bogisichfahey.org/','wisoky.anne@halvorsonblanda.com'),
-        (27,'veritatis','5806 Von Cliff\nTerryhaven, IN 67003-3148','Delfinafurt','NorthDakota',843373,'http://www.cassinbeahan.com/','vullrich@hotmail.com'),
-        (29,'in','6886 Stoltenberg Branch Suite 737\nNorth Antonefort, FL 88898','Port Ransommouth','Missouri',162537,'http://www.beiertromp.net/','arno.wiza@hotmail.com'),
-        (31,'odio','23283 Leon Summit\nArdellashire, FL 00573','Port Nyahside','Nebraska',631,'http://romaguera.net/','fhessel@schowalter.com'),
-        (32,'ut','05902 Crystal Plaza\nDaishabury, RI 13966-1420','Huelburgh','Louisiana',342942,'http://www.willms.com/','ukihn@yahoo.com'),
-        (33,'vel','09889 Balistreri Knoll\nNew Kirsten, VT 68354-0561','Stammtown','SouthCarolina',300,'http://framiwuckert.com/','oral.hermiston@mclaughlin.biz'),
-        (36,'voluptatem','79838 Sally Junctions Apt. 055\nLake Rene, GA 77517','Gertrudemouth','Kansas',913868244,'http://hermiston.com/','lura.howell@kuhlman.biz'),
-        (37,'architecto','6342 Grimes Port\nStarkshire, HI 94322','Keelingburgh','Alabama',651,'http://roob.info/','herdman@murray.biz')
+            (21,'est','1936 O''Reilly Estates Suite 665 East Leonelbury, DC 90308','Port Ileneberg','NewHampshire',0,'http://hudson.info/','weichmann@hotmail.com'),
+            (22,'hic','01200 Lebsack Lakes East Jake, ND 98136','Zemlakchester','California',77,'http://www.bauch.com/','allene29@hotmail.com'),
+            (24,'nihil','14576 Lelah Drive Suite 966 New Chanelhaven, KS 94846','East Jovanyberg','Utah',1179086025,'http://www.kuphal.com/','abraham31@gmail.com'),
+            (25,'at','0975 Jaylan Manor Suite 103 Port Trent, UT 68256-3799','Port Kelliside','NorthCarolina',1,'http://bogisichfahey.org/','wisoky.anne@halvorsonblanda.com'),
+            (27,'veritatis','5806 Von Cliff Terryhaven, IN 67003-3148','Delfinafurt','NorthDakota',843373,'http://www.cassinbeahan.com/','vullrich@hotmail.com'),
+            (29,'in','6886 Stoltenberg Branch Suite 737 North Antonefort, FL 88898','Port Ransommouth','Missouri',162537,'http://www.beiertromp.net/','arno.wiza@hotmail.com'),
+            (31,'odio','23283 Leon Summit Ardellashire, FL 00573','Port Nyahside','Nebraska',631,'http://romaguera.net/','fhessel@schowalter.com'),
+            (32,'ut','05902 Crystal Plaza Daishabury, RI 13966-1420','Huelburgh','Louisiana',342942,'http://www.willms.com/','ukihn@yahoo.com'),
+            (33,'vel','09889 Balistreri Knoll New Kirsten, VT 68354-0561','Stammtown','SouthCarolina',300,'http://framiwuckert.com/','oral.hermiston@mclaughlin.biz'),
+            (36,'voluptatem','79838 Sally Junctions Apt. 055 Lake Rene, GA 77517','Gertrudemouth','Kansas',913868244,'http://hermiston.com/','lura.howell@kuhlman.biz'),
+            (37,'architecto','6342 Grimes Port Starkshire, HI 94322','Keelingburgh','Alabama',651,'http://roob.info/','herdman@murray.biz')
         ON CONFLICT (id, email_id, contact) DO NOTHING;
-    `
+        `
+
 
     return insertedSchools;
 }
