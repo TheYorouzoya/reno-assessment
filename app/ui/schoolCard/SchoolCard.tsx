328d8c1c9ea5083e12b7184fc3c0830427fdd235
Ratnesh Rastogi <ratneshrastogimarch@gmail.com>
2025-11-05 07:44:53 +0000

    Update School Card CSS

--- app/ui/schoolCard/SchoolCard.tsx
+++ app/ui/schoolCard/SchoolCard.tsx
@@ -8,14 +8,14 @@ type SchoolCardProps = {
 export default function SchoolCard({ school } : SchoolCardProps) {
     return (
         <div className="school-card">
-            <section className="card-image">
+            <div className="card-image">
                 <img src={school.image_url} alt={`Image of ${school.name} school`} />
-            </section>
-            <section className="card-body">
+            </div>
+            <div className="card-body">
                 <span className="school-city">{school.city}</span>
                 <h3 className="school-name">{school.name}</h3>
                 <span className="school-address">{school.address}</span>
-            </section>
+            </div>
         </div>
     )
 }
